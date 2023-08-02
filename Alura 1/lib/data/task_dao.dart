b8168e4130751da4519c917e468dd1507d348cb0
nickunger99 <nicolas.unger@yahoo.com.br>
2023-08-02 14:01:32 -0300

    findAll e toList

--- Alura 1/lib/data/task_dao.dart
+++ Alura 1/lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:Tasks/components/task.dart';
+import 'package:Tasks/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql = '''CREATE TABLE $_tablename(
@@ -10,8 +12,27 @@ $_name TEXT, $_difficulty INTEGER, $_image TEXT);''';
   static const String _image = 'image';
 
   save(Task tarefa) async {}
-  Future<List<Task>> findAll() async {}
-  Future<List<Task>> find(String nomeDaTarefa) async{}
-  delete(String nomeDaTarefa) async{}
 
+  Future<List<Task>> findAll() async {
+    print('Acessando o findAll: ');
+    final Database database = await getDatabase();
+    final List<Map<String, dynamic>> result = await database.query(_tablename);
+    print('Procurando dados no banco de dados... encontrado: $result');
+    return toList(result);
+  }
+
+  List<Task> toList(List<Map<String, dynamic>> mapaDeTarefas) {
+    print('Convertendo to List: ');
+    final List<Task> tarefas = [];
+    for (Map<String, dynamic> linha in mapaDeTarefas) {
+      final Task tarefa = Task(linha[_name], linha[_image], linha[_difficulty]);
+      tarefas.add(tarefa);
+    }
+    print('Lista de Tarefas: $tarefas');
+    return tarefas;
+  }
+
+  Future<List<Task>> find(String nomeDaTarefa) async {}
+
+  delete(String nomeDaTarefa) async {}
 }
