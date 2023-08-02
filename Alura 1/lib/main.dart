4d8289ada5cc7f2068082675c7911baee5732ae1
nickunger99 <nicolas.unger@yahoo.com.br>
2023-08-02 13:32:05 -0300

    sqflite, classe DAO e database

--- Alura 1/lib/main.dart
+++ Alura 1/lib/main.dart
@@ -13,13 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First Project',
+      title: 'Tasks',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home:  TaskInherited(
         child: const InitialScreen(),
       ),
+
     );
   }
 }
