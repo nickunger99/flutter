97df6de125cbd0437027814b15c2abff3992b165
nickunger99 <nicolas.unger@yahoo.com.br>
2023-08-03 09:38:51 -0300

    criar, atualizar e deletar

--- Alura 1/lib/data/task_dao.dart
+++ Alura 1/lib/data/task_dao.dart
@@ -11,7 +11,30 @@ $_name TEXT, $_difficulty INTEGER, $_image TEXT);''';
   static const String _difficulty = 'difficulty';
   static const String _image = 'image';
 
-  save(Task tarefa) async {}
+  save(Task tarefa) async {
+    print('Iniciando o save: ');
+    final Database database = await getDatabase();
+    var itemExists = await find(tarefa.nome);
+    Map<String, dynamic> taskMap = toMap(tarefa);
+    if (itemExists.isEmpty) {
+      print('A tarefa não existia.');
+      return await database.insert(_tablename, taskMap);
+    } else {
+      print('A tarefa já existe!');
+      return await database.update(_tablename, taskMap,
+          where: '$_name = ?', whereArgs: [tarefa.nome]);
+    }
+  }
+
+  Map<String, dynamic> toMap(Task tarefa) {
+    print('Convertendo tarefa em Map: ');
+    final Map<String, dynamic> mapaDeTarefas = Map();
+    mapaDeTarefas[_name] = tarefa.nome;
+    mapaDeTarefas[_difficulty] = tarefa.dificuldade;
+    mapaDeTarefas[_image] = tarefa.foto;
+    print('Mapa de Tarefas: $mapaDeTarefas');
+    return mapaDeTarefas;
+  }
 
   Future<List<Task>> findAll() async {
     print('Acessando o findAll: ');
@@ -44,5 +67,10 @@ $_name TEXT, $_difficulty INTEGER, $_image TEXT);''';
     return toList(result);
   }
 
-  delete(String nomeDaTarefa) async {}
+  delete(String nomeDaTarefa) async {
+    print('Deletando tarefa: $nomeDaTarefa');
+    final Database database = await getDatabase();
+    return database
+        .delete(_tablename, where: '$_name = ?', whereArgs: [nomeDaTarefa]);
+  }
 }
