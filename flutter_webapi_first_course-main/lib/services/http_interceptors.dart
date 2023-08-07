9c6b7cbc9d40a31f006657231dc0c8711ec05b01
nickunger99 <nicolas.unger@yahoo.com.br>
2023-08-07 15:15:27 -0300

    API Local e Interceptor

---
+++ flutter_webapi_first_course-main/lib/services/http_interceptors.dart
@@ -0,0 +1,25 @@
+import 'package:http_interceptor/http_interceptor.dart';
+import 'package:logger/logger.dart';
+
+class LoggingInterceptor implements InterceptorContract {
+  Logger logger = Logger();
+
+  @override
+  Future<RequestData> interceptRequest({required RequestData data}) async {
+    print(data.toString());
+    logger.v("Requisição para ${data.baseUrl}\nCabeçalhos: ${data.headers}\nCorpo: ${data.body}");
+    return data;
+  }
+
+  @override
+  Future<ResponseData> interceptResponse({required ResponseData data}) async {
+    if (data.statusCode ~/ 100 == 2) {
+      logger.i(
+          "Resposta de ${data.url}\n${data.headers}\n${data.statusCode}\n ${data.body}");
+    } else {
+      logger.e(
+          "Resposta de ${data.url}\n${data.headers}\n${data.statusCode}\n ${data.body}");
+    }
+    return data;
+  }
+}
